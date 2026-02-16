cae212b76fa8e3130b74769c99ae5f1a30746015
LeNooby09 <contact@lenooby09.tech>
2026-02-16 19:12:24 +0100

    init

---
+++ src/client/kotlin/tech/lenooby09/crackAuth/client/CrackAuthDataGenerator.kt
@@ -0,0 +1,11 @@
+package tech.lenooby09.crackAuth.client
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+class CrackAuthDataGenerator : DataGeneratorEntrypoint {
+
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+		val pack = fabricDataGenerator.createPack()
+	}
+}
---
+++ src/main/kotlin/tech/lenooby09/crackAuth/CrackAuth.kt
@@ -0,0 +1,9 @@
+package tech.lenooby09.crackAuth
+
+import net.fabricmc.api.ModInitializer
+
+class CrackAuth : ModInitializer {
+
+	override fun onInitialize() {
+	}
+}
